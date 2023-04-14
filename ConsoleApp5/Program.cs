448e3651ca8c402ba6ef093c642b5eebaf45a0fe
Snopinio <jansnopkowski64@gmail.com>
2023-04-14 11:37:16 +0200

    cesar szyfr w dwie strony

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,21 +1,63 @@
-﻿//zdefiniuj string do zaszyfrowania
-string clearText = "Ala ma kota, a kot ma Ale, a sierotka ma rysia";
-//zamieniamy na wielkie litery
-clearText = clearText.ToUpper();
-//iteruj przez caly tekst litera po literze
-//deklarujemy string wynikowy
-string encryptedText = "";
-foreach (char c in clearText)
+﻿//deklarujemy funkcje zwracajaca string i przyjmujaca stringa jako argument
+string toAsciiCesar(string clearText)
 {
-    // wyciągamy kod ascii danej litery
-    //w jezyku c# rzutowanie jawne z char na int powoduje pobranie wartosci ascii
-    int asciiCode = (int)c;
-    //dodajemyt wartosc klucza
-    asciiCode += 3;
-    //zamieniamy kod z powrotem na znak
-    char encryptedChar = (char)asciiCode;
-    //dopisujemy do szyfru
-    encryptedText += encryptedChar;
+    //zamieniamy na wielkie litery
+    clearText = clearText.ToUpper();
+    //iteruj przez caly tekst litera po literze
+    //deklarujemy string wynikowy
+    string encryptedText = "";
+    foreach (char c in clearText)
+    {
+        //pomiń wszystkie znaki poza literami z alfabetu lacinskiego
+        if (c >= 65 && c <= 90)
+        {
+            // wyciągamy kod ascii danej litery
+            //w jezyku c# rzutowanie jawne z char na int powoduje pobranie wartosci ascii
+            int asciiCode = (int)c;
+            //dodajemyt wartosc klucza
+            asciiCode += 3;
+            //sprawdzamy czy nie przekroczylismy ostatniego znaku (z,90)
+            if (asciiCode > 90)
+                //jesli przekroczylo to cofnij  o dlugosc alfabetu
+                asciiCode -= 26;
+            //zamieniamy kod z powrotem na znak
+            char encryptedChar = (char)asciiCode;
+            //dopisujemy do szyfru
+            encryptedText += encryptedChar;
+        }
+
+    }
+    //zwroc zakodowany string
+    return encryptedText;
 }
 
-Console.WriteLine("Zaszyfrowany tekst: " + encryptedText);
+string fromAsciiCesar(string encryptedText)
+{
+    string clearText = "";
+    foreach (char c in encryptedText)
+    {
+        if (c >= 65 && c <= 90)
+        {
+            
+            int asciiCode = (int)c;
+           
+            asciiCode -= 3;
+            
+            if (asciiCode > 90)
+               
+                asciiCode -= 26;
+            
+            char encryptedChar = (char)asciiCode;
+           
+            encryptedText += encryptedChar;
+        }
+        string uncryptedText = "";
+    }
+    return encryptedText;
+}
+
+//zdefiniuj string do zaszyfrowania
+string clearText = "Ala ma kota, a kot ma Ale, a sierotka ma rysia";
+
+Console.WriteLine("Odszyfrowany tekst: " + fromAsciiCesar(clearText));
+Console.WriteLine("Zaszyfrowany tekst: " + toAsciiCesar(clearText));
